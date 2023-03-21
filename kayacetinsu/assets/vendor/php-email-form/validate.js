7f93fbb5c14a84dea669e6e6e097eb2a6d0bd9a9
kayacetinsu <126987610+kayacetinsu@users.noreply.github.com>
2023-03-21 15:54:49 -0400

    Add files via upload

---
+++ kayacetinsu/assets/vendor/php-email-form/validate.js
@@ -0,0 +1,103 @@
+/**
+* PHP Email Form Validation - v3.5
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach( function(e) {
+    e.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+ 
+      let action = thisForm.getAttribute('action');
+      // let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+       
+      if( ! action ) {
+        displayError(thisForm, 'The form action property is not set!')
+        return;
+      }
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData( thisForm );
+
+      if(grecaptcha.getResponse().length == 0)
+      {
+          displayError(thisForm, 'Please, complete the reCaptcha before submitting.');
+          return false;
+      }
+
+      formData.delete("g-recaptcha-response");
+      
+      php_email_form_submit(thisForm, action, formData);
+
+      // if ( recaptcha ) {
+      //   if(typeof grecaptcha !== "undefined" ) {
+      //     grecaptcha.ready(function() {
+      //       try {
+      //         grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+      //         .then(token => {
+      //           formData.set('recaptcha-response', token);
+      //           php_email_form_submit(thisForm, action, formData);
+      //         })
+      //       } catch(error) {
+      //         displayError(thisForm, error)
+      //       }
+      //     });
+      //   } else {
+      //     displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+      //   }
+      // } else {
+      //   php_email_form_submit(thisForm, action, formData);
+      // }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: {'X-Requested-With': 'XMLHttpRequest'}
+    })
+    .then(response => {
+      if (!response.ok) {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        throw new Error("HTTP status " + response.status); 
+      }
+      return response.text();
+    })
+    .then(data => {
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      if (data.trim() != '') {
+        thisForm.querySelector('.sent-message').classList.add('d-block');
+        thisForm.reset(); 
+        grecaptcha.reset();
+
+        setTimeout(function() {
+          thisForm.querySelector('.error-message').classList.remove('d-block');
+          thisForm.querySelector('.sent-message').classList.remove('d-block');
+        }, 6000);
+
+      } else {
+        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+      }
+    })
+    .catch((error) => {
+      displayError(thisForm, error);
+    });
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
